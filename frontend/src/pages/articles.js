451ba4144f03397accdbaeb340829e1f34a76149
Abtin Ghods <abtin.ghods@gmail.com>
2020-05-03 19:03:06 -0400

    Fixes a style in articles page

--- frontend/src/pages/articles.js
+++ frontend/src/pages/articles.js
@@ -20,36 +20,38 @@ const ArticlesPage = ({
       <SEO title="Gurdjieff foundation Toronto - Articles" />
       <Container>
         <H1>Articles</H1>
-        <Grid gap={2} columns={1} pt={[3,4,4]}>
+        <Grid gap={3} columns={1} pt={[3, 4, 4]}>
           {articles.map((article, i) => (
             <Grid
-              // gap={2}
+              gap={0}
               onClick={() => navigateToArticle(article.Slug)}
               columns={1}
               key={`article-${article.id}`}
               sx={{
                 display: "inline-grid",
                 bg: "muted",
-                my: [1, 2, 2],
+
                 px: 3,
                 pt: 3,
                 pb: 1,
                 borderRadius: 2,
                 boxShadow: "0 0 2px rgba(0, 0, 0, 0.125)",
-                cursor: 'pointer',
-                '&:hover': {
-                  bg: 'muted-darker',
-                  transition: 'background-color 100ms linear'
-                }
+                cursor: "pointer",
+                "&:hover": {
+                  bg: "muted-darker",
+                  transition: "background-color 100ms linear",
+                },
               }}
             >
               <Grid>
-                <H2 sx={{ color: 'primary' }}>{article.Title}</H2>
+                <H2 sx={{ color: "primary" }}>{article.Title}</H2>
                 <H3>by {article.Author}</H3>
               </Grid>
-              <MDXProvider>
-                <MDXRenderer>{article.childStrapiArticleSummary.childMdx.body}</MDXRenderer>
-              </MDXProvider>
+              <div sx={{ mt: -2 }}>
+                <MDXProvider>
+                  <MDXRenderer>{article.childStrapiArticleSummary.childMdx.body}</MDXRenderer>
+                </MDXProvider>
+              </div>
             </Grid>
           ))}
         </Grid>
