de0e54dbecd1c6fea1376aa32b43e7502552e2f0
Abtin Ghods <abtin.ghods@gmail.com>
2020-05-02 23:21:43 -0400

    Adds latest articles sections to home page

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,6 +1,6 @@
 /** @jsx jsx */
-import { jsx } from "theme-ui"
-import { graphql } from "gatsby"
+import { jsx, Grid, Heading, Divider } from "theme-ui"
+import { graphql, navigate } from "gatsby"
 import { Flex, Text } from "theme-ui"
 import Img from "gatsby-image"
 import { MDXProvider } from "@mdx-js/react"
@@ -12,52 +12,76 @@ import SEO from "../components/seo"
 const IndexPage = ({
   data: {
     strapiHomePage: { quote, childStrapiHomePagecontent, contentImage, childStrapiHomePageLinks },
+    allStrapiArticle: { nodes: articles },
   },
-}) => (
-  <Layout>
-    <SEO title="Gurdjieff foundation Toronto" />
-    <div sx={{ mt: -2, width: "100%", bg: "muted", color: "primary" }}>
-      <Flex
-        sx={{
-          variant: "container.margin",
-          justifyContent: "center",
-          alignItems: ["start", "center", "center"],
-          py: [3, 4, 5],
-        }}
-      >
-        <Text
-          sx={{
-            fontSize: 6,
-            fontFamily: "quote",
-          }}
-          as="blockquote"
-        >
-          " {quote} "
-        </Text>
-      </Flex>
-    </div>
+}) => {
+  const navigateToArticle = slug => {
+    navigate(`/articles/${slug}`)
+  }
 
-    <div sx={{ variant: "container.margin", mt: 5 }}>
-      <Img
-        sx={{ float: "left ", mr: [4, 4, 5], mb: [3, 3, 4], width: 350 }}
-        fluid={contentImage.childImageSharp.fluid}
-      />
-      <div>
-        <MDXProvider>
-          <MDXRenderer>{childStrapiHomePagecontent.childMdx.body}</MDXRenderer>
-        </MDXProvider>
+  return (
+    <Layout>
+      <SEO title="Gurdjieff foundation Toronto" />
+      <div sx={{ mt: -2, width: "100%", bg: "muted", color: "primary" }}>
+        <Flex
+          sx={{
+            variant: "container.margin",
+            justifyContent: "center",
+            alignItems: ["start", "center", "center"],
+            py: [3, 4, 5],
+          }}
+        >
+          <Text
+            sx={{
+              fontSize: 6,
+              fontFamily: "quote",
+            }}
+            as="blockquote"
+          >
+            " {quote} "
+          </Text>
+        </Flex>
       </div>
-    </div>
-    {/* <div sx={{ mt: 7, bg: 'secondary', '& p, & h1, & h2, & h3': {color: 'background'}, '& a': { color: 'primary-darker'} }}> */}
-    <div sx={{ mt: 7, bg: 'secondary', '& a': { color: 'primary-darker'} }}>
-      <div sx={{ variant: "container.margin", py: 3 }}>
-        <MDXProvider>
-          <MDXRenderer>{childStrapiHomePageLinks.childMdx.body}</MDXRenderer>
-        </MDXProvider>
+
+      <div sx={{ variant: "container.margin", mt: 5 }}>
+        <Img
+          sx={{ float: "left ", mr: [4, 4, 5], mb: [3, 3, 4], width: 350 }}
+          fluid={contentImage.childImageSharp.fluid}
+        />
+        <div>
+          <MDXProvider>
+            <MDXRenderer>{childStrapiHomePagecontent.childMdx.body}</MDXRenderer>
+          </MDXProvider>
+        </div>
       </div>
-    </div>
-  </Layout>
-)
+      <div sx={{ variant: "container.margin" }}>
+        <Divider sx={{ mt: 7, mb: 5, color: "muted" }} />
+        <Heading as="h1">Latest Articles</Heading>
+        <Grid gap={1} columns={1} sx={{ mt: [3, 3, 4] }}>
+          {articles.map((article, i) => (
+            <Flex key={`contact-us-${article.id}`} onClick={() => navigateToArticle(article.Slug)}>
+              <Text as="span" sx={{ color: "primary", cursor: "pointer" }}>
+                {article.Title}
+              </Text>
+              <Text as="span" sx={{ ml: 2 }}>
+                by {article.Author}
+              </Text>
+            </Flex>
+          ))}
+        </Grid>
+      </div>
+
+      {/* <div sx={{ mt: 7, bg: 'secondary', '& p, & h1, & h2, & h3': {color: 'background'}, '& a': { color: 'primary-darker'} }}> */}
+      <div sx={{ mt: 6, bg: "secondary", "& a": { color: "primary-darker" } }}>
+        <div sx={{ variant: "container.margin", py: 3 }}>
+          <MDXProvider>
+            <MDXRenderer>{childStrapiHomePageLinks.childMdx.body}</MDXRenderer>
+          </MDXProvider>
+        </div>
+      </div>
+    </Layout>
+  )
+}
 
 export default IndexPage
 
@@ -71,10 +95,10 @@ export const query = graphql`
         }
       }
       childStrapiHomePageLinks {
-      childMdx {
-        body
+        childMdx {
+          body
+        }
       }
-    }
       contentImage {
         id
         relativePath
@@ -87,6 +111,13 @@ export const query = graphql`
             ...GatsbyImageSharpFixed
           }
         }
+      }
+    }
+    allStrapiArticle {
+      nodes {
+        Title
+        Author
+        Slug
       }
     }
   }
